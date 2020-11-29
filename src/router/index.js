4bf537e3c18786e190c0eac67a024f0ba9c9cf76
brkysever <brkysever@gmail.com>
2020-11-29 15:45:49 +0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [{
+        path: '/',
+        name: 'NEWS',
+        component: NEWS
+    },
+    {
+        path: '/REVIEWS',
+        name: 'REVIEWS',
+        component: () =>
+            import ( /* webpackChunkName: "profile" */ '../views/REVIEWS'),
+    },
+
+    {
+        path: '/PHONES',
+        name: 'PHONES',
+        component: () =>
+            import ( /* webpackChunkName: "profile" */ '../views/PHONES'),
+    },
+    {
+        path: '/VIDEOS',
+        name: 'VIDEOS',
+        component: () =>
+            import ( /* webpackChunkName: "profile" */ '../views/VIDEOS'),
+    }
+];
+
+const router = new VueRouter({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
+});
+
+export default router;
