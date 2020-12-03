c07f95fd3358ef331e3fde4d4d54d71fb7224729
brkysever <brkysever@gmail.com>
2020-12-03 22:28:19 +0300

    news tamamlandı

---
+++ src/components/Post.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="post">
+    
+      <div>
+        <img class="news-img" :src="postImage">
+        </div>
+
+          <div class="xx">
+            <a class="text"> {{ postText }} </a>
+                <br>
+            <a class="date"> {{ postDate }} </a>
+          </div>
+        
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Post',
+    components: {},
+    props:{
+        postText:{
+            type: String
+        },
+        postImage:{
+            type: String
+        },
+        postDate:{
+            type: String
+        }
+    }
+}
+
+</script>
+
+<style>
+.post {
+  padding-top: 50px;
+  display: flex;
+  flex-direction:row;
+}
+
+.news-img{  
+   margin-left: 10px;
+   width: 170px;
+   height: 110px;
+   border: 1px solid #000000;
+   box-shadow: 1px 1px 5px grey;
+}
+
+.xx{
+   margin-left: 20px;
+   width: 150px;
+}
+.date{
+   color: grey;
+   font-size: 10px;
+}
+
+</style>
--- src/views/NEWS/index.vue
+++ src/views/NEWS/index.vue
@@ -1,93 +1,65 @@
 <template>
   <div class="news">
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-       Dolorem nisi blanditiis magnam maiores! Exercitationem itaque placeat blanditiis 
-       rerum ad ex ipsum sed officiis, dicta id accusamus dolore quas nemo recusandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi blanditiis
-         magnam maiores! Exercitationem itaque placeat blanditiis rerum ad ex ipsum sed officiis, dicta 
-         id accusamus dolore quas nemo recusandae?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-       Dolorem nisi blanditiis magnam maiores! Exercitationem itaque placeat blanditiis 
-       rerum ad ex ipsum sed officiis, dicta id accusamus dolore quas nemo recusandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi blanditiis
-         magnam maiores! Exercitationem itaque placeat blanditiis rerum ad ex ipsum sed officiis, dicta 
-         id accusamus dolore quas nemo recusandae?</p>
-  
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-       Dolorem nisi blanditiis magnam maiores! Exercitationem itaque placeat blanditiis 
-       rerum ad ex ipsum sed officiis, dicta id accusamus dolore quas nemo recusandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi blanditiis
-         magnam maiores! Exercitationem itaque placeat blanditiis rerum ad ex ipsum sed officiis, dicta 
-         id accusamus dolore quas nemo recusandae?</p>
-  
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-       Dolorem nisi blanditiis magnam maiores! Exercitationem itaque placeat blanditiis 
-       rerum ad ex ipsum sed officiis, dicta id accusamus dolore quas nemo recusandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi blanditiis
-         magnam maiores! Exercitationem itaque placeat blanditiis rerum ad ex ipsum sed officiis, dicta 
-         id accusamus dolore quas nemo recusandae?</p>
-  
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-       Dolorem nisi blanditiis magnam maiores! Exercitationem itaque placeat blanditiis 
-       rerum ad ex ipsum sed officiis, dicta id accusamus dolore quas nemo recusandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi blanditiis
-         magnam maiores! Exercitationem itaque placeat blanditiis rerum ad ex ipsum sed officiis, dicta 
-         id accusamus dolore quas nemo recusandae?</p>
-  
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-       Dolorem nisi blanditiis magnam maiores! Exercitationem itaque placeat blanditiis 
-       rerum ad ex ipsum sed officiis, dicta id accusamus dolore quas nemo recusandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi blanditiis
-         magnam maiores! Exercitationem itaque placeat blanditiis rerum ad ex ipsum sed officiis, dicta 
-         id accusamus dolore quas nemo recusandae?</p>
-  
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-       Dolorem nisi blanditiis magnam maiores! Exercitationem itaque placeat blanditiis 
-       rerum ad ex ipsum sed officiis, dicta id accusamus dolore quas nemo recusandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi blanditiis
-         magnam maiores! Exercitationem itaque placeat blanditiis rerum ad ex ipsum sed officiis, dicta 
-         id accusamus dolore quas nemo recusandae?</p>
-  
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-       Dolorem nisi blanditiis magnam maiores! Exercitationem itaque placeat blanditiis 
-       rerum ad ex ipsum sed officiis, dicta id accusamus dolore quas nemo recusandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi blanditiis
-         magnam maiores! Exercitationem itaque placeat blanditiis rerum ad ex ipsum sed officiis, dicta 
-         id accusamus dolore quas nemo recusandae?</p>
-  
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-       Dolorem nisi blanditiis magnam maiores! Exercitationem itaque placeat blanditiis 
-       rerum ad ex ipsum sed officiis, dicta id accusamus dolore quas nemo recusandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi blanditiis
-         magnam maiores! Exercitationem itaque placeat blanditiis rerum ad ex ipsum sed officiis, dicta 
-         id accusamus dolore quas nemo recusandae?</p>
-  
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-       Dolorem nisi blanditiis magnam maiores! Exercitationem itaque placeat blanditiis 
-       rerum ad ex ipsum sed officiis, dicta id accusamus dolore quas nemo recusandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi blanditiis
-         magnam maiores! Exercitationem itaque placeat blanditiis rerum ad ex ipsum sed officiis, dicta 
-         id accusamus dolore quas nemo recusandae?</p>
-  
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-       Dolorem nisi blanditiis magnam maiores! Exercitationem itaque placeat blanditiis 
-       rerum ad ex ipsum sed officiis, dicta id accusamus dolore quas nemo recusandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi blanditiis
-         magnam maiores! Exercitationem itaque placeat blanditiis rerum ad ex ipsum sed officiis, dicta 
-         id accusamus dolore quas nemo recusandae?</p>
-  
+     
+   <Post
+   v-for="post of postData"
+   :key="post.mediaID"
+   :postImage="post.postImage"
+   :postText="post.postText"
+   :postDate="post.postDate"
+   />
 
-  
   </div>
 </template>
 
 <script>
+import Post from '@/components/Post'
+
+
+const RESPONSE = [
+  {
+    postText: 'The Poco M3 is rumored to launch in Europe at around €150, will have a 48 MP camera.',
+    postDate: '1 hours ago.',
+    postImage:'https://i.imgur.com/8Xww2J0.jpg'
+  },
+    {
+    postText: 'Redmi K30 5G starts receiving Android 11-based MIUI 12.',
+    postDate: '2 hours ago.',
+    postImage:'https://i.imgur.com/WolwIwz.jpg'
+  },
+    {
+    postText: 'Oppo is holding off on building rollable phones commercially.',
+    postDate: '3 hours ago.',
+    postImage:'https://i.imgur.com/CPHS34r.jpg'
+  },
+    {
+    postText: 'Report: the Huawei P50 will use Kirin 9000 chips, Samsung and LG OLED displays.',
+    postDate: '4 hours ago.',
+    postImage:'https://i.imgur.com/mA4NYAK.jpg'
+  },
+    {
+    postText: 'The Poco M3 is rumored to launch in Europe at around €150, will have a 48 MP camera.',
+    postDate: '9 hours ago.',
+    postImage:'https://i.imgur.com/uN8qi9K.jpg'
+  }
+
+]
+
 
 export default {
-name: 'News',
-components: {
- 
-}
+  name: 'News',
+  components: {
+     Post
+  },
+
+data() {
+    return {
+      postData: []
+    }
+  },
+  async created() {
+    this.postData = RESPONSE
+  }
 }
 </script>
 
@@ -95,3 +67,4 @@ components: {
 
 
 </style>
+
